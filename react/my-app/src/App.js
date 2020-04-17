9fd4ea9a1b1ffa07e1ffa6c46a97646e8ac5826e
halopend <jean.fradet@me.com>
2020-04-17 17:21:05 -0400

    Confidence Slider display

--- react/my-app/src/App.js
+++ react/my-app/src/App.js
@@ -16,6 +16,14 @@ function App() {
             </div>
             <div id="updatenote" class="updatenote mt10"> loading model ..</div>
           </div>
+          <div class="slidecontainer flexfull  ">
+            <input type="range" val="70" min="1" max="100" class="slider" id="confidencerange"></input>
+          </div>
+          {/* <form oninput="result.value=parseInt(b.value)">
+            <input type="range" name="b" value="70" min="1" max="100" class="slider" />
+            <output name="result"></output>
+          </form> */}
+
           <video class="videobox canvasbox" autoplay="autoplay" id="myvideo"></video>
           <canvas id="canvas" class="border canvasbox"></canvas>
           <script src="index.js"></script>
